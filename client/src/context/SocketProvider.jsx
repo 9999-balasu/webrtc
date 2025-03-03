e9e502ff0b91f2c57cc276b602b6c8a3b591d9d0
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-03-03 21:41:52 +0530

    change

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -10,7 +10,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io('localhost:8000'), []);
+    const socket = useMemo(() => io(process.env.REACT_APP_SOCKET_SERVER), []);
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
