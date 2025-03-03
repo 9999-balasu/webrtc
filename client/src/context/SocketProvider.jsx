23e7f48afb38ccb7ad710af8c3d0b361a6fbd06f
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-03-03 21:30:50 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -10,7 +10,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io('https://webrtc-backend-theta.vercel.app/'), []);
+    const socket = useMemo(() => io('localhost:8000'), []);
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
